40dff2862007a94d6f3f1e4c277ffdfcd2fdeceb
Shyam177 <82257160+Shyam177@users.noreply.github.com>
2021-06-24 22:05:24 +0530

    Update MainController.java

--- src/main/java/com/demo/project/devopstraining/controller/MainController.java
+++ src/main/java/com/demo/project/devopstraining/controller/MainController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Main  controller {
+public class MainController {
 	
 	@GetMapping("/")
 	public String getmessage() {
